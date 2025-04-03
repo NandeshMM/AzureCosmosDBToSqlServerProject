e02adc4d45c97007c3df8d81b644ba38a54b13cc
NandeshMM <nandeshsahyadri@gmail.com>
2025-04-03 14:53:54 +0530

    DTO Push

---
+++ DataStore.Abstraction/IDTO/IQueryParameterDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStore.Abstraction.IDTO
+{
+    public interface IQueryParameterDTO
+    {
+        string ContainerName { get; set; }
+        string TableName { get; set; }
+        string CompanyId { get; set; }
+        string UserId { get; set; }
+    }
+}
---
+++ DataStore.Abstraction/IRepositories/ICosmosDBDataFetchingRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStore.Abstraction.IRepositories
+{
+    public interface ICosmosDBDataFetchingRepository
+    {
+        Task<List<dynamic>> GetDataFromCosmosDBAsync(int batchsize = 1000, QueryParameterDTO queryparameters);
+    }
+}
