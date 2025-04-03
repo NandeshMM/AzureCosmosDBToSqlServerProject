35824ef6c7ecdf99e3b19cd1aac24ab05b45f670
ganigadarshith <ganigadarshith@gmail.com>
2025-04-03 15:06:04 +0530

    darshit demo changes

---
+++ DataStore.Implementation/QueryParameterDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStore.Implementation
+{
+    public  class QueryParameterDTO
+    {
+
+        public string Name { get; set; }
+
+
+
+    }
+}
---
+++ FeatureIObjects.Implementation/Datatransferservice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeatureIObjects.Implementation
+{
+    public class Datatransferservice
+    {
+        private readonly CosmosDbRepository _cosmosrepo;
+        private readonly SqlServerRepository _sqlRepo;
+            private readonly BlobstorageService _blobService;
+
+        public Datatransferservice(CosmosDbRepository cosmosrepo, SqlServerRepository sqlRepo, BlobstorageService blobService)
+        {
+            _blobService = blobService;
+            _sqlRepo = sqlRepo;
+             _cosmosrepo = cosmosrepo;
+        }
+
+        public async Task TransferDataAsync()
+        {
+            var records= await _cosmosrepo.GetAllRecordsAsync();
+
+
+        }
+
+
+    }
+}
